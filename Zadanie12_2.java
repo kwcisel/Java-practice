c733864511dd572c401ab5f6a15731fc2869a721
kwcisel <kamila.wcisel@gmail.com>
2016-06-04 20:18:14 +0200

    Add files via upload

---
+++ Zadanie12_2.java
@@ -0,0 +1,16 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+import java.awt.Color;
+
+public
+	class Zadanie12_2 extends Applet{
+		
+		public static void main(String[] args){
+			
+		}
+		
+		public void paint(Graphics g){
+			g.setColor(Color.blue);
+			g.fillOval(50, 100, 50, 50);
+	}	
+}

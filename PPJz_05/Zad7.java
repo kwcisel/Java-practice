258f4052c409476b4676f4e82cffe0ee4e97d144
kwcisel <kamila.wcisel@gmail.com>
2016-12-13 23:00:11 +0100

    Add files via upload

---
+++ PPJz_05/Zad7.java
@@ -0,0 +1,25 @@
+package zadania;
+
+public class Zad7 {
+
+	public static void main(String[] args) {
+
+		int licznik = 0;
+		int liczba = 2;
+		int x = 2;
+
+		while (licznik < 10) {
+			if (liczba % x == 0) {
+				if (liczba == x) {
+					System.out.println("Liczba " + liczba + " jest pierwsza.");
+					licznik++;
+					liczba++;
+				} else {
+					liczba++;
+				}
+			} else {
+				x++;
+			}
+		}
+	}
+}

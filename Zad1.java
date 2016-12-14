57cd1dcea345e4412fc49666944f9018ca66ab21
kwcisel <kamila.wcisel@gmail.com>
2016-12-14 19:42:48 +0100

    Add files via upload

---
+++ Zad1.java
@@ -0,0 +1,14 @@
+package zadania07;
+
+public class Zad1 {
+
+	public static void main(String[] args) {
+		String[] slowa = {
+				"Ala", "kota", "ma", "ma", "a", "kot", "Ale"
+		};
+		
+		System.out.println(slowa[0] + " " + slowa[2] + " " + slowa[1] + " " + slowa[4] + " " + slowa[5] + " " + slowa[3] + " " + slowa[6]);
+
+	}
+
+}

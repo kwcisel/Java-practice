35dcadcb54a01714c27c39330603763c30392222
kwcisel <kamila.wcisel@gmail.com>
2016-12-15 20:12:24 +0100

    Add files via upload

---
+++ PPJz_10/Zad1/ZbiorMetod.java
@@ -0,0 +1,13 @@
+package Zad1;
+
+public class ZbiorMetod {
+
+	public static void ustalWartosc(int wrt){
+		System.out.println("Typ = int, wartosc = " + wrt);
+	}
+	
+	public static void ustalWartosc(float wrt){
+		System.out.println("Typ = float, wartosc = " + wrt);
+	}
+	
+}

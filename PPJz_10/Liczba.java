9b92492e18dea47d2571fe25a1e10a30cf752430
kwcisel <kamila.wcisel@gmail.com>
2016-12-15 20:12:54 +0100

    Add files via upload

---
+++ PPJz_10/Liczba.java
@@ -0,0 +1,15 @@
+package Zad2;
+
+public class Liczba {
+
+	public int wrt;
+	
+	public void przypiszWartosc(int wrt){
+		this.wrt = wrt;
+	}
+	
+	public void wyswietlWartosc(int wrt){
+		System.out.println(wrt);
+	}
+	
+}

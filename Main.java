591a6c99937422d1cc9fb0f84ca3bc2fc372507d
kwcisel <kamila.wcisel@gmail.com>
2016-12-15 20:11:49 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+package Zad1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int wrt1 = 5;
+		float wrt2 = 2.1f;
+		char wrt3 = 'c';
+		byte wrt4 = 2;
+		
+		ZbiorMetod.ustalWartosc(wrt1);
+		ZbiorMetod.ustalWartosc(wrt2);
+		ZbiorMetod.ustalWartosc(wrt3);
+		ZbiorMetod.ustalWartosc(wrt4);
+
+	}
+
+}
